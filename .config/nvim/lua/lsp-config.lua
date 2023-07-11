4597f2de944d2ed08b6cd383c0e997ed5e0add3e
Jonathan Boeckel <me@jonnyb.name>
2023-07-11 23:13:36 +0200

    Configure lsp config

--- .config/nvim/lua/lsp-config.lua
+++ .config/nvim/lua/lsp-config.lua
@@ -4,7 +4,43 @@ lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
 end)
 
--- (Optional) Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+require('mason-lspconfig').setup()
+
+require('mason-tool-installer').setup {
+  ensure_installed = {
+    'lua-language-server',
+    'selene',   -- Lua stylecheck
+    'stylua',
+  }
+}
+
+require('lspconfig').lua_ls.setup({
+  root_dir = function()
+    --- project root will be the first directory that has
+    --- either .luarc.json or .stylua.toml
+    return lsp.dir.find_first({'.luarc.json', '.stylua.toml'})
+  end
+})
+
+-- require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
+
+local null_ls = require("null-ls")
+
+local builtins = null_ls.builtins
+local code_actions = builtins.code_actions
+local completion = builtins.completion
+local diagnostics = builtins.diagnostics
+local formatting = builtins.formatting
+
+null_ls.setup({
+  sources = {
+    code_actions.gitsigns,
+    completion.spell,
+    diagnostics.eslint,
+    diagnostics.selene,
+    formatting.stylua,
+  }
+})
+
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,19 +3,20 @@ local lsp_zero = {
   branch = 'v2.x',
   dependencies = {
     -- LSP Support
-    {'neovim/nvim-lspconfig'},
+    'neovim/nvim-lspconfig',
     {
       'williamboman/mason.nvim',
       build = function()
         pcall(vim.cmd, 'MasonUpdate')
       end,
     },
-    {'williamboman/mason-lspconfig.nvim'},
+    'williamboman/mason-lspconfig.nvim',
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
 
     -- Autocompletion
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'L3MON4D3/LuaSnip'},
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-nvim-lsp',
+    'L3MON4D3/LuaSnip',
   }
 }
 
@@ -24,17 +25,6 @@ local null_ls = {
   'jose-elias-alvarez/null-ls.nvim',
   dependencies = { 'nvim-lua/plenary.nvim', 'williamboman/mason.nvim' },
   event = { "BufReadPre", "BufNewFile" },
-  config = function() 
-    local null_ls = require("null-ls") 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.code_actions.gitsigns,
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.completion.spell,
-      }
-    })
-  end
 }
 
 local trouble = {
