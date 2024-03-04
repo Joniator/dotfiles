a92694c60ce7f18aa49f4e52aaa37d4b2dbf54c9
Jonathan Boeckel <jonathan.boeckel@msg.group>
2024-03-04 16:37:39 +0100

    Add win32yank

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -154,19 +154,19 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- If win32yank is installed, assume we are in WSL and want to use the windows clipboard
-if vim.fn.executable('win32yank.exe') then
-  vim.g.clipboard = {
-    name = WslClipboard,
-    copy = {
-      ["+"] = "win32yank.exe -i --crlf",
-      ["*"] = "win32yank.exe -i --crlf",
-    },
-    paste = {
-      ["+"] = "win32yank.exe -o --lf",
-      ["*"] = "win32yank.exe -o --lf",
-    },
-    cache_enabled = 0,
-  }
+if vim.fn.executable("win32yank.exe") then
+	vim.g.clipboard = {
+		name = WslClipboard,
+		copy = {
+			["+"] = "win32yank.exe -i --crlf",
+			["*"] = "win32yank.exe -i --crlf",
+		},
+		paste = {
+			["+"] = "win32yank.exe -o --lf",
+			["*"] = "win32yank.exe -o --lf",
+		},
+		cache_enabled = 0,
+	}
 end
 
 -- [[ Install `lazy.nvim` plugin manager ]]
