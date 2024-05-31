9732d6bb9f7e88351bafb3a5d6fa298f478aad1e
Jonathan Boeckel <jboeckel@mailbox.org>
2024-05-31 19:34:46 +0000

    Update, add colors

--- dot_config/nvim/lua/jonnyb/plugins/appearance.lua
+++ dot_config/nvim/lua/jonnyb/plugins/appearance.lua
@@ -1,4 +1,10 @@
 return {
+  { 'NvChad/nvim-colorizer.lua', opts = {
+    user_default_options = {
+      css = true,
+      tailwind = true,
+    },
+  } },
   {
     'catppuccin/nvim',
     name = 'catppuccin',
