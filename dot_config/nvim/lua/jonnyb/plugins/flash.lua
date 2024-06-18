05df7c86cc369fd2d979606c8d9ebdad4ef88154
Jonathan Boeckel <jboeckel@mailbox.org>
2024-06-18 10:57:12 +0200

    Configure flash

--- dot_config/nvim/lua/jonnyb/plugins/flash.lua
+++ dot_config/nvim/lua/jonnyb/plugins/flash.lua
@@ -2,5 +2,19 @@ return {
   'folke/flash.nvim',
   event = 'VeryLazy',
   ---@type Flash.Config
-  opts = {},
+  opts = {
+    modes = {
+      char = {
+        jump_labels = true,
+        highlight = { backdrop = false },
+      },
+    },
+    label = {
+      rainbow = {
+        enabled = true,
+        -- number between 1 and 9
+        shade = 5,
+      },
+    },
+  },
 }
