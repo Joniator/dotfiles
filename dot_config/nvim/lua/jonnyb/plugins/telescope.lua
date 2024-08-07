6dac41e52bbbb021fd8416099d403c97ebd0e7f1
Jonathan Boeckel <jboeckel@mailbox.org>
2024-08-07 18:56:50 +0200

    Search more hidden files

--- dot_config/nvim/lua/jonnyb/plugins/telescope.lua
+++ dot_config/nvim/lua/jonnyb/plugins/telescope.lua
@@ -39,12 +39,18 @@ return {
 
       local builtin = require 'telescope.builtin'
       local map = vim.keymap.set
+      local find_files = function()
+        builtin.find_files { hidden = true }
+      end
+      local live_grep = function()
+        builtin.live_grep { additional_args = { '--hidden' } }
+      end
       map('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       map('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      map('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      map('n', '<leader>sf', find_files, { desc = '[S]earch [F]iles' })
       map('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       map('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      map('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      map('n', '<leader>sg', live_grep, { desc = '[S]earch by [G]rep' })
       map('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       map('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       map('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
