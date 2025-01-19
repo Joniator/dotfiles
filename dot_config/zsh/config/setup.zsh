7bf4d2f47087ddad782d3e5ad53a3e59fad0d0af
Jonathan Boeckel <jboeckel@mailbox.org>
2025-01-19 09:36:46 +0000

    Don't setup everything on apply

--- dot_config/zsh/config/setup.zsh
+++ dot_config/zsh/config/setup.zsh
@@ -4,7 +4,9 @@ fast-theme -q $XDG_CACHE_HOME/antidote/catppuccin/zsh-fsh/themes/catppuccin-macc
 
 GO_INSTALL_DIR="/usr/local/go"
 if [[ -d "$GO_INSTALL_DIR" ]]; then export PATH="$GO_INSTALL_DIR/bin:$PATH"; fi
+
 if [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]]; then source "$SDKMAN_DIR/bin/sdkman-init.sh"; fi
+
 if [[ -d "$HOME/.cargo/env" ]]; then source "$HOME/.cargo/env"; fi
 
 if [[ -d "$ASDF_DIR" ]]; then source "$ASDF_DIR/asdf.sh"; fi
@@ -13,9 +15,10 @@ FNM_PATH="$HOME/.local/share/fnm"
 if [[ -d $FNM_PATH ]]; then export PATH="$FNM_PATH:$PATH"; eval "`fnm env`"; fi
 
 if command -v fzf &> /dev/null; then source <(fzf --zsh); fi
-bindkey "^R" atuin-search # Override fzf ctrl-r with historized atuin
+
+if command -v atuin &> /dev/null; then bindkey "^R" atuin-search; fi
 
 if command -v zoxide &> /dev/null; then eval "$(zoxide init --cmd cd zsh)"; fi
 
-eval "$(oh-my-posh init zsh --config $XDG_CONFIG_HOME/omp/jonnyb.omp.yaml)"
+if command -v oh-my-posh &> /dev/null; then eval "$(oh-my-posh init zsh --config $XDG_CONFIG_HOME/omp/jonnyb.omp.yaml)"; fi
 
