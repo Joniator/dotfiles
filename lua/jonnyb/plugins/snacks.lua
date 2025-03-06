9ddf9fca4fc4b1a0c28b36e2ec89957fb10d042e
Jonathan Boeckel <jonathan.boeckel@msg.group>
2025-03-06 17:49:47 +0100

    More java

--- lua/jonnyb/plugins/snacks.lua
+++ lua/jonnyb/plugins/snacks.lua
@@ -1,10 +1,32 @@
 return {
-    "folke/snacks.nvim",
-    ---@type snacks.Config
-    opts = {
-        dashboard = {
-            preset = {
-                header = [[
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    dashboard = {
+      preset = {
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+          },
+          {
+            icon = " ",
+            key = "s",
+            desc = "Sessions",
+            action = function()
+              MiniSessions.select()
+            end,
+          },
+          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+        header = [[
 .==============================================.
 |                                              |
 |                           .'\                |
@@ -37,7 +59,7 @@ return {
 |                                              |
 '=============================================='
 Art by myflix ]],
-            },
-        },
+      },
     },
+  },
 }
