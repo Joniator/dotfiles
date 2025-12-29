645a89f584035336d39e145d22ff024c828d4a50
Jonathan Boeckel <jboeckel@mailbox.org>
2025-12-29 18:00:32 +0100

    Update and pin treesitter

--- dot_config/nvim/lua/jonnyb/plugins/treesitter.lua
+++ dot_config/nvim/lua/jonnyb/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   { -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
+    branch = "master",
     build = ":TSUpdate",
     dependencies = {
       "nvim-treesitter/nvim-treesitter-context",
