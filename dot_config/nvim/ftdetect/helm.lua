79ff89318c6cbbf92ec8ea9137a94b53bf174359
Jonathan Boeckel <jonathan.boeckel@msg.group>
2026-07-20 11:50:58 +0200

    Fix helm and add mr pushes

--- dot_config/nvim/ftdetect/helm.lua
+++ dot_config/nvim/ftdetect/helm.lua
@@ -1,7 +1,9 @@
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   desc = "Detect helmfiles",
   pattern = { "*/helm/**/*.yaml", "*/helm/**/*.yml", "*/helm/**/*.tpl" },
-  callback = function()
-    vim.api.nvim_set_option_value("ft", "helm", {})
+  callback = function(arg)
+    if arg.file ~= "values.yaml" then
+      vim.api.nvim_set_option_value("ft", "helm", {})
+    end
   end,
 })
